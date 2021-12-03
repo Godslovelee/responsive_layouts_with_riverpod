4efb79d217b6234fa3d58eed9a7c3482d19dca89
Godslovelee <godslovelee@yahoo.com>
2021-12-03 05:27:43 +0100

    "imported flutter_riverpod main.dart"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_layouts_w_riverpod/first_page.dart';
 import 'app_menu.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
