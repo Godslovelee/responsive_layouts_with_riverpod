eebbe20611aef70ba1004f9782455aa75c21a010
Godslovelee <godslovelee@yahoo.com>
2021-12-21 23:33:51 +0100

    ""

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Dddedddddddddddddddd',
+      title: 'Flutter Dddedddddddddddddddddd',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
